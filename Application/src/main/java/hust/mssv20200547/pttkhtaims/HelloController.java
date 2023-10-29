c4b2459ecac971c85c15b9a2010971cb8b1e81a7
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-10-29 22:02:38 +0700

    init source code

---
+++ Application/src/main/java/hust/mssv20200547/pttkhtaims/HelloController.java
@@ -0,0 +1,14 @@
+package hust.mssv20200547.pttkhtaims;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ Application/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module hust.mssv20200547.pttkhtaims {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens hust.mssv20200547.pttkhtaims to javafx.fxml;
+    exports hust.mssv20200547.pttkhtaims;
+}
