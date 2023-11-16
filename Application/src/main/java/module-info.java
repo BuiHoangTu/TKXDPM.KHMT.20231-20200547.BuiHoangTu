4b55be49b45ebc0c0b5840baff569ec890fe8fd9
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-11-16 19:44:44 +0700

    delete module-info.java to use lombok

--- Application/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module hust.mssv.pttkhtaims {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires lombok;
-
-
-    opens hust.mssv20200547.pttkhtaims to javafx.fxml;
-    exports hust.mssv20200547.pttkhtaims;
-}
