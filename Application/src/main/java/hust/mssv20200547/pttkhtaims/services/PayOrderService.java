a7844952a15b77cca822df42b9d97b4d88a2bb98
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-11-18 10:57:06 +0700

    implementing PayOrderService

--- Application/src/main/java/hust/mssv20200547/pttkhtaims/services/PayOrderService.java
+++ Application/src/main/java/hust/mssv20200547/pttkhtaims/services/PayOrderService.java
@@ -1,24 +1,28 @@
 package hust.mssv20200547.pttkhtaims.services;
 
+import hust.mssv20200547.pttkhtaims.models.Card;
+
+import java.util.regex.Pattern;
+
 public class PayOrderService implements IPayOrderService {
 
     @Override
     public boolean validateCardHolderName(String cardHolderName) {
-        return false;
+        return Card.validateCardHolderName(cardHolderName);
     }
 
     @Override
     public boolean validateCardNumber(String cardNumber) {
-        return false;
+        return Card.validateCardNumber(cardNumber);
     }
 
     @Override
     public boolean validateCardExpirationDate(String expirationDate) {
-        return false;
+        return Card.validateCardExpirationDate(expirationDate);
     }
 
     @Override
     public boolean validateCardSecurityCode(String securityCode) {
-        return false;
+        return Card.validateCardSecurityCode(securityCode);
     }
 }
